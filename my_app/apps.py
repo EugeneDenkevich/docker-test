580cf969d9f2663941e8445c5be4f8e319bf4754
eugene <eugenestudio@mail.ru>
2023-06-23 08:39:33 +0300

    create simple flask app

---
+++ my_app/apps.py
@@ -0,0 +1,12 @@
+from flask import Flask
+from flask import render_template
+from markupsafe import escape
+
+
+app = Flask(__name__)
+
+
+@app.route('/')
+@app.route('/<name>')
+def hello_world(name=None):
+    return render_template('index.html', name=name)
